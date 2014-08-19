8cce66801057ef3f9a70de6c514b5136ee01df88
Yavor Ivanov <davko996@gmail.com>
2014-08-19 18:09:13 +0300

    Small Edit

--- Java Basics/Java-Sintax-HW/src/_01_CalculateArea.java
+++ Java Basics/Java-Sintax-HW/src/_01_CalculateArea.java
@@ -4,7 +4,6 @@ public class _01_CalculateArea {
     public static void main(String args[]){
         System.out.println("Enter 'a' and 'b':");
         Scanner input = new Scanner(System.in);
-
         int a = input.nextInt(), b = input.nextInt();
         System.out.println(a * b);
     }
